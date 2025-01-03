3bd18c106988ce33d8eacd53456162f966ccec83
Sanvir Manilal <sanvir.manilal@gmail.com>
2025-01-03 15:36:55 +0100

    inject httpclient

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,15 @@ import { HeaderComponent } from "./layout/header/header.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.http.get(this.baseUrl + 'products').subscribe({
+      next: data => console.log(data),
+      error: error => console.error(error),
+      complete: () => console.log('complete')
+    });
+  }
+  baseUrl = 'https://localhost:5001/api/'
+  private http = inject(HttpClient)
   title = 'skinet';
 }
