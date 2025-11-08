2f47b2d494bc980ec01c33fe399c988de6c3ba69
Sanvir Manilal <sanvir.manilal@gmail.com>
2025-11-08 15:58:48 +0100

    Add Entity Framework Core setup and initial entity models
    - Added DbContext and connection string configuration for SQL Server.
    - Introduced BaseEntity and Product entity classes.
    - Updated project files to include necessary EF Core packages.
    - Removed unused Class1 files from Core and Infrastructure projects.

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,13 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<StoreContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 app.MapControllers();
