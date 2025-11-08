3b3abf4340ed264a7773418f77e0807e39aad35f
Sanvir Manilal <sanvir.manilal@gmail.com>
2025-11-08 16:49:05 +0100

    Add Product entity and configuration, update connection string, and initialize migrations

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Core.Entities;
 
 public class Product : BaseEntity
@@ -6,6 +8,8 @@ public class Product : BaseEntity
     public required string Description { get; set; }
     public required string Name { get; set; }
     public required string PictureUrl { get; set; }
+
+    [DataType("decimal(18,2)")]
     public decimal Price { get; set; }
     public int QuantityInStock { get; set; }
     public required string Type { get; set; }
